ad0ff5fb2da13cee6fb89368ae2b1d3dfdb5ab78
coed95 <codecoed95@gmail.com>
2022-12-22 20:27:47 +0100

    fixed a typo

--- 1/1-3.c
+++ 1/1-3.c
@@ -1,4 +1,4 @@
-/* Exercise 1-3. Modify the tremperature conversion program to print a heading
+/* Exercise 1-3. Modify the temperature conversion program to print a heading
    above the table. */
 
 #include <stdio.h>
