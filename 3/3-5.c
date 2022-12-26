e6d734a976ac5d9dbb5bb0621585896f60304731
coed95 <codecoed95@gmail.com>
2022-12-26 18:23:01 +0100

    3-5

--- 3/3-5.c
+++ 3/3-5.c
@@ -1,5 +1,5 @@
 /*
-	Exercise 3-5. Write the function itob(n, s, b) that converts the integer n into a base b character representation in the string s. In particular, itob(n, s, 16) formats n as a hexadecimal integer in s. (For now I just made it for unsigned ints only)
+Exercise 3-5. Write the function itob(n, s, b) that converts the integer n into a base b character representation in the string s. In particular, itob(n, s, 16) formats n as a hexadecimal integer in s. (For now I just made it for unsigned ints only)
 */
 
 #include <stdio.h>
