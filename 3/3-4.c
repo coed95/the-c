1e2b901870b4aad3b97a62c6702cc7451ac765b9
coed95 <codecoed95@gmail.com>
2022-12-26 06:24:16 +0100

    itoa to accept the largest negative number

---
+++ 3/3-4.c
@@ -0,0 +1,30 @@
+/*
+ 	Exercise 3-4. In a two's complement number representation, our version of itoa doesn't 	handle the largest negative number, that is, the value of n equal to -(2^wordsize-1). 	Explain why not. Modify it to print that value correctly, regardless of the machine on 	which it runs.
+*/
+
+/*
+ 	Explanation:
+
+	if ((sign = n) < 0)
+		n = -n;
+
+	doesn't work for the largest negative number because it is one greater compared to the positive one. To make it work, it is possible to do the opposite control: if the sign is positive, make it negative, and then check it back at the end of the do-while loop.
+*/
+
+void itoa(int n, char s[])
+{
+	int sign, i = 0;
+
+	if ((sign = n) > 0)
+		n = -n;				/* make it temporarily negative */
+
+	do {
+		s[i++] = '0' - (n % 10);
+	} while ((n /= 10) < 0);
+
+	if (sign < 0)
+		s[i++] = '-';
+
+	s[i] = '\0';
+	reverse(s);
+}
