0fd80b5508130173a07ac361ab33b72963c3c2cd
coed95 <codecoed95@gmail.com>
2023-01-02 18:26:29 +0100

    5-1

---
+++ 5/5-1.c
@@ -0,0 +1,60 @@
+/* Exercise 5-1. As written, getint treat a + or - not followed by a digit as a valid representation of zero.
+Fix it to push such a character back on the input. */
+
+#include <ctype.h>
+
+#define		BUFSIZE		100
+
+char buf[BUFSIZE];					/* buffer for ungetch */
+int bufp = 0;						/* next free position in buf */
+
+int getch(void);					/* get a (possibly pushed back) character */
+void ungetch(int);					/* ungetch: push a character back on input */
+int getint(int *pn);				/* getint: get next integer from input into *pn */
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int ch)
+{
+	if (bufp >= BUFSIZE)
+		puts("error: too many characters");
+	else
+		buf[bufp++] = ch;
+}
+
+int getint(int *pn)
+{
+	int c, sign;
+
+	while (isspace(c = getch()))	/* skip white spaces */
+		;
+
+	if (!isdigit(c) && c != EOF)
+	{
+		ungetch(c);		/* not a number */
+		return 0;
+	}
+
+	sign = (c == '-') ? -1 : 1;
+
+	if (c == '+' || c == '-') {
+		c = getch();
+
+		if (!isdigit(c)) {
+			ungetch((sign == 1) ? '+' : '-');
+			return 0;
+		}
+	}
+
+	for (*pn = 0; isdigit(c); c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+
+	if (c != EOF)
+		ungetch(c);
+
+	return c;
+}
