88014d0bc90fe58f27422355c547f13100fc26cd
coed95 <codecoed95@gmail.com>
2023-01-03 15:56:06 +0100

    pointer version of strcat(s, t)

---
+++ 5/5-3.c
@@ -0,0 +1,24 @@
+/* Exercise 5-3. Write a pointer version of the function strcat(s, t), that copies the string t to the end of s. */
+
+#include <stdio.h>
+
+void pstrcat(char *, char *);
+
+int main(void)
+{
+	char test[12] = "hello ";
+	pstrcat(test, "world");
+
+	printf("%s\n", test);
+
+	return 0;
+}
+
+void pstrcat(char *s, char *t)
+{
+	for (; *s; s++)
+		;
+
+	while (*s++ = *t++)
+		;
+}
