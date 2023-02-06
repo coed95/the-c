bd1d31b525e8936a1a14ad02446f5ad94007a374
coed95 <codecoed95@gmail.com>
2023-02-06 08:25:35 +0100

    implemented isupper

--- 7/7-9.c
+++ 7/7-9.c
@@ -1,6 +1,22 @@
 /* Exercise 7-9. Functions like isupper can be implemented to save space or to save time. */
 
-#include <ctype.h>
+#include <stdio.h>
+
+#define isupper(x)	(x >= 'A' && x <= 'Z')
+
+int isstrupper(char *);
+
+int main(int argc, char *argv[])
+{
+	char *str = (argc == 1) ? "DEFAULT" : argv[1];
+
+	if (isstrupper(str))
+		printf("%s is upper\n", str);
+	else
+		printf("%s is not upper\n", str);
+
+	return 0;
+}
 
 int isstrupper(char *str)
 {
